9b84ee902170bf0f239f78050b60856d865c2653
ei12159 <ei12159@fe.up.pt>
2015-05-23 15:00:25 +0100

    added TOP_U function

--- RecommenderSystem/src/tars/RecommendationProcess.java
+++ RecommenderSystem/src/tars/RecommendationProcess.java
@@ -2,6 +2,10 @@ package tars;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+import utilities.Pair;
 import utilities.TreeNode;
 
 public class RecommendationProcess {
@@ -35,38 +39,67 @@ public class RecommendationProcess {
 		
 		Collections.sort(tf, Collections.reverseOrder());
 		for(int i=0; i<tf.size();i++){
-			System.out.println("tf = "+tf.get(i).getProb());
+			System.out.println("tf = "+tf.get(i).getNode().getUserObject()+"-"+tf.get(i).getProb());
 		}
 	}
 	
 	public double prob(double trustIntensity){
-		//System.out.println("ti = "+trustIntensity);
 		return Math.max(trustIntensity, 1/level);
 	}
 	
-	public void aggregatingRatings(Double [][]ratings){
-		ArrayList<Integer> top_U = top_U(dtp.getTrustMatrix(),1);
-		//System.out.println("top_U = " + top_U.size());
+	public TreeNode aggregatingRatings(Double [][]ratings){
+		ArrayList<Integer> top_U = top_U(dtp.getTrustMatrix(),1, 10);
+		System.out.println("top_U = " + top_U.size());
 		if(top_U.size()==1){
-			
-		}else{
-			
+			return tf.get(0).getNode();
 		}
+		
+		TreeNode temp=null;
+		double maior=-1;
+		for(int i=0; i<tf.size();i++){
+			for(int j=0; j<top_U.size(); j++){
+				if(tf.get(i).getNode().getUserObject()==top_U.get(j)){
+					if(tf.get(i).getProb()>=maior){
+						maior=tf.get(i).getProb();
+						temp=tf.get(i).getNode();
+						System.out.println("temp = "+tf.get(i).getNode().getUserObject());
+					}
+				}
+			}
+		}
+		
+		return temp;
 	}
 	
-	public ArrayList<Integer> top_U(Double [][] trustMatrix, int item){
+	public ArrayList<Integer> top_U(Double [][] trustMatrix, int item, int n){
+		
+		ArrayList<Pair<Integer, Double> > temp = new ArrayList<Pair<Integer, Double> >();
 		ArrayList<Integer> top_U = new ArrayList<Integer>();
-		double maior = -1;
 		
 		for(int i = 0; i<trustMatrix.length;i++){
-			if(trustMatrix[i][item]!=null && trustMatrix[i][item]>=maior){
-				maior=trustMatrix[i][item];
+			if(trustMatrix[i][item] != null){
+				temp.add(new Pair<Integer, Double>(i, trustMatrix[i][item]));
 			}
 		}
-		for(int i = 0; i<trustMatrix.length;i++){
-			if(trustMatrix[i][item]!=null && trustMatrix[i][item]==maior){
-				top_U.add(i);
+		
+		Collections.sort(temp, new Comparator<Pair<Integer, Double>>() {
+			@Override
+			public int compare(Pair<Integer, Double> o1, Pair<Integer, Double> o2) {
+				if (o1.getSecond() == null && o2.getSecond() == null) {
+					return 0;
+				}
+				if (o1.getSecond() == null) {
+					return 1;
+				}
+				if (o2.getSecond() == null) {
+					return -1;
+				}
+				return o1.getSecond().compareTo(o2.getSecond()) * (-1);
 			}
+		});
+		int num = n<temp.size()?n:temp.size();
+		for(int i = 0; i<num;i++){
+			top_U.add(temp.get(i).getFirst());
 		}
 		
 		return top_U;
